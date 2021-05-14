9c8d83ea8313439c2f812217a1a785a763dfedab
JuJu Train <jooyoung0878@users.noreply.github.com>
2021-05-14 21:30:42 +0900

    React modular app

---
+++ recipe-app/src/components/IngredientsList.js
@@ -0,0 +1,10 @@
+import Ingredient from './Ingredient'
+
+const IngredientList = ({ list }) =>
+  <ul className="ingredients">
+    {list.map((ingredient, i) =>
+      <Ingredient key={i} {...ingredient} />
+    )}
+  </ul>
+
+export default IngredientList
